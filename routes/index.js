25b699996d41e2a7d804be775f91af21b26e0b6f
arielgreen73 <arielkarmit@gmail.com>
2022-05-24 10:50:52 +0300

    changes in routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.send(
+    "this is the home page. use /customers/home /products/home or /orders/home."
+  );
+});
+
+router.get("/chat", function (req, res, next) {
+  const filePath = path.join(__dirname, "../client", "chat.html");
+  res.sendFile(filePath);
 });
 
 module.exports = router;
