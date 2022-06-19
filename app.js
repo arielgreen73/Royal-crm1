f924de6c90355bc3cbf477e0a697b2906f720b26
arielgreen73 <arielkarmit@gmail.com>
2022-06-19 17:58:43 +0300

    add client in angular

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import {
+  Application,
+  Request,
+  Response,
+  NextFunction,
+  ErrorRequestHandler,
+} from "express";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const createError = require("http-errors");
+
+const auth = require("./middleware/auth");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var customersRouter = require("./routes/customers");
+var productsRouter = require("./routes/products");
+var ordersRouter = require("./routes/orders");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "client")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", auth, usersRouter);
+app.use("/customers", customersRouter);
+app.use("/products", auth, productsRouter);
+app.use("/orders", auth, ordersRouter);
+
+// catch 404 err forward error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// custom error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.locals.message = err.message;
+  res.locals.error = err;
+
+  res.status(500).send(err);
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+import { Request, Response, NextFunction } from "express";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const express = require("express");
+const router = express.Router();
+const mwAuth = require("../middleware/auth");
+const auth = require("../controllers/auth");
+const fileMgmt = require("../shared/fileMgmt");
+
+/* authentication */
+router.get("/signin", function (req, res, next) {
+  const filePath = fileMgmt.getHtmlFilePath("login.html");
+  res.sendFile(filePath);
+});
+
+router.post("/login", auth.login);
+
+router.get("/logout", mwAuth, function (req, res, next) {
+  return res
+    .clearCookie("access_token")
+    .status(200)
+    .send("Successfully logged out.");
+});
+
+/* home page */
+router.get("/", function (req, res, next) {
+  res.send(
+    "this is the home page. use /customers/home /products/home or /orders/home."
+  );
+});
+
+/* chat */
+router.get("/chat", mwAuth, function (req, res, next) {
+  const filePath = fileMgmt.getHtmlFilePath("chat.html");
+  res.sendFile(filePath);
 });
 
 module.exports = router;
